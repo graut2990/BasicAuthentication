c246a30e2ad1352a5209dc0b1d76fbda50837acb
Gaurav Raut <42151702+graut2990@users.noreply.github.com>
2019-03-22 18:04:45 +0530

    Add files via upload

---
+++ AspDotNetCoreWebApi_BasicAuthentication_Demo/AspDotNetCoreWebApi_BasicAuthentication_Demo/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspDotNetCoreWebApi_BasicAuthentication_Demo.IRepository;
+using AspDotNetCoreWebApi_BasicAuthentication_Demo.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCoreWebApi_BasicAuthentication_Demo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]User userParam)
+        {
+            var user = await _userRepository.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+       
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _userRepository.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await _userRepository.Get(id);
+            return Ok(user);
+        }
+
+    }
+}
---
+++ AspDotNetCoreWebApi_BasicAuthentication_Demo/AspDotNetCoreWebApi_BasicAuthentication_Demo/IRepository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspDotNetCoreWebApi_BasicAuthentication_Demo.Model;
+
+namespace AspDotNetCoreWebApi_BasicAuthentication_Demo.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<User> Authenticate(string username, string password);
+        Task<IEnumerable<User>> GetAll();
+
+        Task<User> Get(int id);
+    }
+}
